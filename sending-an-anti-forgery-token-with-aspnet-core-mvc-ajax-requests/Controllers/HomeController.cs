eb9b11eae1235da8c5dbe5fd97d200be6c380be7
plbonneville <pier_lucb@hotmail.com>
2020-06-28 16:29:40 -0400

    Added the code sample for the 'Sending an anti-forgery token with ASP.NET Core MVC AJAX requests' post.

---
+++ sending-an-anti-forgery-token-with-aspnet-core-mvc-ajax-requests/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SendingAntiForgeryTokenWithAspnetCoreMvcAjaxRequests.Models;
+
+namespace SendingAntiForgeryTokenWithAspnetCoreMvcAjaxRequests.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        [HttpGet("{api}")]
+        public IActionResult Index(string api = "jquery")
+        {
+            var allowedApis = new []{ "jquery", "fetch", "axios" };
+
+            if (!allowedApis.Contains(api.ToLower()))
+            {
+                throw new Exception("Invalid API.");
+            }
+
+            ViewData["Title"] = api;
+            return View($"Index-{api}");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult PostAjaxData(PersonViewModel personViewModel)
+        {
+            // Todo: Save person to database
+            return Json(new { Id = personViewModel.Id });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult PostAjaxJson([FromBody] PersonViewModel personViewModel)
+        {
+            // Todo: Save person to database
+            return Json(new { Id = personViewModel.Id });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sending-an-anti-forgery-token-with-aspnet-core-mvc-ajax-requests/Models/PersonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendingAntiForgeryTokenWithAspnetCoreMvcAjaxRequests.Models
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Firstname { get; set; }
+
+        [Required]
+        public string Lastname { get; set; }
+    }
+}
