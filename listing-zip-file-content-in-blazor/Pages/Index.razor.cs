dff1abf1d1ec984dd143f0fe2829eac84e5530aa
plbonneville <9940262+plbonneville@users.noreply.github.com>
2020-11-11 15:33:35 -0500

    New post
    -Listing zip file content in Blazor

---
+++ listing-zip-file-content-in-blazor/Pages/Index.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorZipExplorer.Pages
+{
+public partial class Index
+{
+    private const string DefaultStatus = "Choose a zip file";
+
+    private List<ZipEntry> _entries;
+    private string _fileName;
+
+    private string _status = DefaultStatus;
+
+    [Inject] private ZipService ZipService { get; set; }
+
+    private async Task OnInputFileChange(InputFileChangeEventArgs e)
+    {
+        await using var stream = e.File.OpenReadStream();
+
+        _entries = await ZipService.ExtractFiles(stream);
+        _fileName = e.File.Name;
+
+        _status = DefaultStatus;
+    }
+}
+}
---
+++ listing-zip-file-content-in-blazor/ZipService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorZipExplorer
+{
+    public class ZipService
+    {
+        public async Task<List<ZipEntry>> ExtractFiles(Stream fileData)
+        {
+            await using var ms = new MemoryStream();
+            await fileData.CopyToAsync(ms);
+
+            using var archive = new ZipArchive(ms);
+
+            var entries = new List<ZipEntry>();
+
+            foreach (var entry in archive.Entries)
+            {
+                await using var fileStream = entry.Open();
+                var fileBytes = await fileStream.ReadFully();
+                var content = Encoding.UTF8.GetString(fileBytes);
+
+                entries.Add(new ZipEntry { Name = entry.FullName, Content = content });
+            }
+
+            return entries;
+        }
+    }
+
+    public static class StreamExtension
+    {
+        public static async Task<byte[]> ReadFully(this Stream input)
+        {
+            await using var ms = new MemoryStream();
+            await input.CopyToAsync(ms);
+            return ms.ToArray();
+        }
+    }
+}
