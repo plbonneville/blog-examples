e22017dc7e4c055b59d99822e48889dc40941d5c
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2022-12-24 13:20:04 -0500

    Migrated to the new minimal hosting model

--- sending-an-anti-forgery-token-with-aspnet-core-mvc-ajax-requests/Program.cs
+++ sending-an-anti-forgery-token-with-aspnet-core-mvc-ajax-requests/Program.cs
@@ -1,10 +1,27 @@
-using SendingAntiForgeryTokenWithAspnetCoreMvcAjaxRequests;
+var builder = WebApplication.CreateBuilder(args);
 
-CreateHostBuilder(args).Build().Run();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
-static IHostBuilder CreateHostBuilder(string[] args) =>
-   Host.CreateDefaultBuilder(args)
-       .ConfigureWebHostDefaults(webBuilder =>
-       {
-           webBuilder.UseStartup<Startup>();
-       });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
