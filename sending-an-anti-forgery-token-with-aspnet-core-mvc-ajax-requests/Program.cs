cbeb562a44a8aeaca57863f632e801350572dced
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2022-12-24 13:00:53 -0500

    Converted to top level statements

--- sending-an-anti-forgery-token-with-aspnet-core-mvc-ajax-requests/Program.cs
+++ sending-an-anti-forgery-token-with-aspnet-core-mvc-ajax-requests/Program.cs
@@ -1,16 +1,10 @@
-namespace SendingAntiForgeryTokenWithAspnetCoreMvcAjaxRequests;
+using SendingAntiForgeryTokenWithAspnetCoreMvcAjaxRequests;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+CreateHostBuilder(args).Build().Run();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+static IHostBuilder CreateHostBuilder(string[] args) =>
+   Host.CreateDefaultBuilder(args)
+       .ConfigureWebHostDefaults(webBuilder =>
+       {
+           webBuilder.UseStartup<Startup>();
+       });
